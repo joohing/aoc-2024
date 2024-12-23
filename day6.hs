61fbbd79a4ab9a8c939d0e3040276e50a8529b3d
jonathansamsing <jonathan.samsing@boostplm.com>
2024-12-23 19:24:31 +0100

    Kinda sorta works but its slow so I'm deleting it.

--- day6.hs
+++ day6.hs
@@ -7,15 +7,17 @@ import Debug.Trace
 import Control.Monad
 import Util
 
-data Guard = Guard Point Dir
+-- Guard (Position, Walking direction, Is looping)
+data Guard = Guard Point Dir Bool
 data Point = Point Int Int deriving Eq
 data Dir = FaceUp | FaceDown | FaceLeft | FaceRight deriving Eq
-newtype AreaMap = AreaMap [String] deriving Show
+newtype AreaMap = AreaMap [String]
 
 dir_chars = "^v<>"
 
 instance Show Guard where
-    show (Guard (Point x y) d) = "Guard((" ++ show x ++ ", " ++ show y ++ "), " ++ show d ++ ")"
+    show (Guard (Point x y) d looping) =
+        "Guard((" ++ show x ++ ", " ++ show y ++ "), " ++ show d ++ ", " ++ show looping ++ ")"
 
 instance Show Dir where
     show d = case d of
@@ -26,6 +28,12 @@ instance Show Dir where
 
 instance Show Point where
     show (Point x y) = "(x: " ++ show x ++ ", y: " ++ show y ++ ")"
+
+instance Show AreaMap where
+    show (AreaMap am) = "AreaMap:\n" ++ intercalate "\n" am
+
+-- Remove X's and direction chars.
+cleanup_x = map $ \c -> if (c == 'X' || c `elem` dir_chars) then '.' else c
 
 -- p1 minus p2.
 subtract :: Point -> Point -> Point
@@ -61,17 +69,19 @@ find_char c strs
 get_guard :: AreaMap -> Maybe Guard
 get_guard am
     | null m = trace "No map given." Nothing
-    | found /= -1 = Just $ Guard (Point found 0) FaceUp
-    | otherwise = Just $ Guard (Point next_x (1 + next_y)) next_dir
+    | found /= -1 = Just $ Guard (Point found 0) FaceUp False
+    | otherwise = Just $ Guard (Point next_x (1 + next_y)) next_dir looping
     where
         AreaMap m = am
         found = idxof '^' (head m)
-        Just (Guard (Point next_x next_y) next_dir) = get_guard $ AreaMap (tail m)
+        Just (Guard (Point next_x next_y) next_dir looping) = get_guard $ AreaMap (tail m)
 
 -- Is the point in bounds of the map?
 p_in_bounds :: Point -> AreaMap -> Bool
-p_in_bounds p am = (0 <= x && x < length m) && (0 <= y && y < (length $ head m))
+p_in_bounds p am = xinb && yinb
     where
+        xinb = (0 <= x && x < (length $ head m))
+        yinb = (0 <= y && y < length m)
         Point x y = p
         AreaMap m = am
 
@@ -88,7 +98,7 @@ char_at_point p am
 -- If you got nothing, you left the map. Or gave it wrong parameters.
 next_collision :: Point -> Dir -> AreaMap -> Maybe Point
 next_collision p d am
-    | not (p_in_bounds p am)            = trace "Point not in bounds, no collision." Nothing
+    | not (p_in_bounds p am)            = Nothing
     | char_at_point p am == Just '#'    = Just p
     | d == FaceLeft                     = next_collision (Point (x - 1) y) d am
     | d == FaceDown                     = next_collision (Point x (y + 1)) d am
@@ -101,8 +111,7 @@ next_collision p d am
 -- Return a new map with the given point painted over with an X.
 draw_point :: Char -> Point -> AreaMap -> Maybe AreaMap
 draw_point c p am
-    | null m = Nothing
-    | not (p_in_bounds p am) = Nothing
+    | null m || not (p_in_bounds p am) = Nothing
     | otherwise = Just $ AreaMap (top ++ ([midleft ++ c:midright]) ++ bot)
     where
         AreaMap m = am
@@ -118,11 +127,12 @@ draw_line :: Point -> Point -> AreaMap -> Maybe AreaMap
 draw_line p1 p2 am
     | null m = Nothing
     | not (p_in_bounds p1 am) = Just am
-    | (char_at_point p1 am >>= is_dir_char) == Just True = next_draw am
-    | p1 == p2 = draw_point 'X' p1 am
+    | p1 == p2 = if (curr_is_dir_char == Just True) then Just am else draw_point 'X' p1 am
+    | curr_is_dir_char == Just True = next_draw am
     | otherwise = draw_point 'X' p1 am >>= next_draw
     where
         is_dir_char = \c -> Just $ c `elem` dir_chars
+        curr_is_dir_char = (char_at_point p1 am >>= is_dir_char)
         AreaMap m = am
         Point x1 y1 = p1
         Point x2 y2 = p2
@@ -173,34 +183,38 @@ get_path_slice p1 p2 am
         AreaMap m = am
         next_p = get_delta_point p1 p2
 
--- Map a drawn-on map (a map with a guard's path) to a list of new maps, where each
+-- Map a 'default map' and a drawn-on map (a map with a guard's path) to a list of new maps, where each
 -- square the guard has touched is mapped to a new obstacle.
-map_to_loop_attempts :: AreaMap -> Maybe [AreaMap]
-map_to_loop_attempts am
+map_to_loop_attempts :: AreaMap -> AreaMap -> Maybe [AreaMap]
+map_to_loop_attempts dm am
     | null m || isNothing found = Nothing
     | found /= Nothing          = do
-        found   :: Point <- found
-        next_am :: AreaMap <- draw_point '.' found am
-        this_am :: AreaMap <- draw_point '#' found am
-        rest    :: [AreaMap] <- map_to_loop_attempts next_am
-        return (this_am:rest)
+        found    :: Point <- found
+        next_am  :: AreaMap <- draw_point '.' found am
+        this_am  :: AreaMap <- draw_point '#' found dm
+        start    :: Point <- start_point
+        this_am' :: AreaMap <- draw_point '.' start this_am
+        let rest :: Maybe [AreaMap] = map_to_loop_attempts dm next_am
+        return (this_am':(if isNothing rest then [] else (let (Just rest') = rest in rest')))
     where
-        AreaMap m = am
-        found     = find_char 'X' m
+        AreaMap m   = am
+        cleaned am  = let AreaMap m = am in AreaMap $ map (\s -> cleanup_x s) m
+        found       = find_char 'X' m
+        start_point = find_char '^' m
 
 -- Same as above but put down markers for when the guard changes directions. Used for loop detection.
 walk_map :: Guard -> AreaMap -> Maybe (Guard, AreaMap)
 walk_map g am
-    | char_at_point curr_pos am == (Just '#') = Nothing
-    | not (p_in_bounds curr_pos am) = Just (g, am)
-    | (Just $ char_from_dir curr_dir) == (char_at_point curr_pos am) = Just (g, am)
+    | char_at_point curr_pos am == (Just '#') = trace "walk_map: guard inside obstacle" $ Nothing
+    | not (p_in_bounds curr_pos am) = trace "walk_map: guard oob" $ Just (g, am)
+    | (Just $ char_from_dir curr_dir) == (char_at_point curr_pos am) = Just (Guard curr_pos curr_dir True, am)
     | otherwise = do
         next_pos <- if next_coll == Nothing then edge else next_coll
         next_map <- draw_line curr_pos next_pos am
-        next_map' <- draw_point (char_from_dir next_dir) curr_pos next_map
-        return (Guard next_pos next_dir, next_map')
+        next_map' <- draw_point (char_from_dir curr_dir) curr_pos next_map
+        return (Guard next_pos (rotate_right curr_dir) looping, next_map')
     where
-        Guard curr_pos curr_dir = g
+        Guard curr_pos curr_dir looping = g
         coll = next_collision curr_pos curr_dir am
         next_coll = coll >>= (\c -> Just $ approach_obstacle c curr_dir)
         next_dir = rotate_right curr_dir
@@ -209,20 +223,29 @@ walk_map g am
 -- Walk until the guard is off the mapped area.
 keep_walking :: Guard -> AreaMap -> Maybe (Guard, AreaMap)
 keep_walking g am
-    | not $ p_in_bounds g_pos am = Just (g, am)
-    | char_at_point g_pos am == Just (char_from_dir g_dir) = Just (g, am) -- It loops
+    | not $ p_in_bounds g_pos am || looping = trace "guard looping or oob" $ Just (g, am)
+    | char_here `elem` dir_chars = Just (Guard g_pos g_dir True, am) -- It loops
     | otherwise = walk_map g am >>= (\t -> keep_walking (fst t) (snd t))
     where
-        Guard g_pos g_dir = g
+        Guard g_pos g_dir looping = g
         AreaMap m = am
+        Just char_here = char_at_point g_pos am
+        char_currdir = Just (char_from_dir g_dir)
+
+-- -- Takes an AreaMap with turns on it and
+-- filter_loops :: AreaMap -> AreaMap
+
+-- Did the guard loop?
+did_loop :: Guard -> Bool
+did_loop g = let Guard _ _ looped = g in looped
 
 -- I flipped something somewhere idk
 char_from_dir :: Dir -> Char
 char_from_dir d = case d of
-    FaceUp      -> 'v'
-    FaceDown    -> '^'
-    FaceLeft    -> '>'
-    FaceRight   -> '<'
+    FaceUp      -> '^'
+    FaceDown    -> 'v'
+    FaceLeft    -> '<'
+    FaceRight   -> '>'
 
 task1 input = do
     handle <- openFile input ReadMode
@@ -233,21 +256,42 @@ task1 input = do
     let Just start = find_char '^' lines
     let Just am' = draw_point '.' start am
     let Just g = get_guard am
-    let Just (Guard p d, AreaMap walked) = keep_walking g am'
-    prall walked
+    let Just (Guard p d looped, AreaMap walked) = keep_walking g am'
 
     let replace = map $ \c -> if c `elem` dir_chars then 'X' else c
     let walked' = map (\s -> replace s) walked
 
     print ("Task 1 result: " ++ show (sum $ map (count 'X') walked'))
 
--- Fix the input before running task1 on it.
 task2 input = do
     handle <- openFile input ReadMode
     contents <- hGetContents handle
 
-    print ("Task 2 result: not done")
+    let lines = split '\n' contents
+    let am = AreaMap lines
+    let Just start = find_char '^' lines
+    let Just am' = draw_point '.' start am
+    let Just g = get_guard am
+    let Just (Guard p d looped, AreaMap walked) = keep_walking g am'
+
+    let replace = map $ \c -> if c `elem` dir_chars then 'X' else c
+    let AreaMap walked' = AreaMap $ map (\s -> replace s) walked
+    let Just (AreaMap walked'') = draw_point '^' start (AreaMap walked')
+
+    -- walked'' has all X's and the start marker. Now replace this with a list of AreaMaps where
+    -- each has one X replaced with # and the rest replaced.
+    -- There are 4756 of these in the input.
+    let Just attempts = map_to_loop_attempts am (AreaMap walked'')
+
+    -- Now map to keep_walking.
+    let keep_walked = catMaybes $ map (keep_walking g) attempts
+    let guard_states = map fst keep_walked
+    let enumerated_gs = zip [0..] (map fst keep_walked)
+    prall enumerated_gs
+    let loop_count = length $ filter did_loop guard_states
+
+    print ("Task 2 result: " ++ show loop_count)
 
 main = do
     -- task1 "input/day6-input.txt"
-    task2 "input/day6-sample.txt"
+    task2 "input/day6-input.txt"
